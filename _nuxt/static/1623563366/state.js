d80214e746233547543743944bc9022a6e6b104b
Loïc Gamache <lgamache@nmedia.ca>
2021-06-13 01:51:21 -0400

    publish

---
+++ _nuxt/static/1623563366/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhunt-showdown-randomizer\u002F_nuxt\u002Fstatic\u002F1623563366",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhunt-showdown-randomizer\u002F",assetsPath:"\u002Fhunt-showdown-randomizer\u002F_nuxt\u002F",cdnURL:a}}}}(null));
